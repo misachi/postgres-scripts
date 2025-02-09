3e09eda3c8e8b8d5d1667d0a54161161b411f315
Brian Misachi <misachibrian@gmail.com>
2025-02-09 20:39:05 +0300

    Exit gracefully if we find pg has already been downloaded
    Reduce size of pg downloaded to depth=1

--- scripts/install/pg.sh
+++ scripts/install/pg.sh
@@ -3,9 +3,9 @@
 check_pg_exists() {
     if [ -d "postgres" ]; then
         echo "postgres directory already exists"
-        exit 1
+        exit 0
     fi
 }
 
 check_pg_exists
-git clone https://github.com/postgres/postgres.git
+git clone --depth=1 https://github.com/postgres/postgres.git
